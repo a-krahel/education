93ff1bfc673c39f3d940b6a383aaa1834ec96f80
a.krahel <artur.krahel@outlook.com>
2023-02-09 13:20:35 +0300

    feat: configured eslint & prettier, add .env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/app';
 
 @Module({
-  imports: [],
   controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.development.env', '.local.env'],
+      load: [configuration],
+    }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
