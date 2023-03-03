17420f9c743e5743731e723385fe94c3cb145019
a.krahel <artur.krahel@outlook.com>
2023-03-03 09:48:31 +0300

    feat: update config file

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,13 +1,24 @@
 import * as process from 'process';
 
+import { Users } from '../users/users.model';
+
 const env = process.env;
+
 export default () => ({
   database: {
     database: env.DB_DATABASE,
+    dialect: env.DB_DIALECT,
     host: env.DB_HOST,
+    models: [Users],
     password: env.DB_PASSWORD,
-    port: env.DB_PORT,
-    user: env.DB_USER,
+    port: parseInt(env.DB_PORT),
+    username: env.DB_USER,
+  },
+  jwt: {
+    secret: 'topSecret51',
+    signOptions: {
+      expiresIn: env.JWT_LIFETIME,
+    },
   },
   port: parseInt(env.NODE_PORT) || 3000,
 });
